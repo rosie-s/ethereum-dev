c8f85d960069dcfffcb6ded5ee7ed1a7e427f418
rosie-s <rosie-s@users.noreply.github.com>
2020-10-22 10:08:43 +0200

    feat: Project Setup

---
+++ eth-todo-list/contracts/TodoList.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.22 <0.8.0;
+
+contract TodoList {
+    // State variables are written to the blockchain
+    // State of the smart contract on the blockchain
+    uint public taskCount = 0; // read value from smart contract using public
+}
